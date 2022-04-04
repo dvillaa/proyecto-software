697a6cdd6336487fea65020138b97f022d818aeb
david <david@LAPTOP-AAL16LQT>
2022-04-04 11:12:38 +0200

    conexion Cliente

--- Ajedrez/src/main/java/Cliente/ModeloEnLinea/Cliente.java
+++ Ajedrez/src/main/java/Cliente/ModeloEnLinea/Cliente.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package Cliente.ModeloEnLinea;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  *
  * @author david
  */
 public class Cliente {
+    final String HOST = "";
+    final int PUERTO = 5500;
+    DataInputStream in;
+    DataOutputStream out;
     
+    public Cliente() throws IOException {
+        Socket sc = new Socket(HOST, PUERTO);
+        
+        in = new DataInputStream(sc.getInputStream());
+        out = new DataOutputStream(sc.getOutputStream());
+        
+        out.writeUTF("Hola desde el cliente");
+        
+        String mensaje = in.readUTF();
+        
+        System.out.println(mensaje);
+        
+        sc.close();
+        
+    }    
 }
